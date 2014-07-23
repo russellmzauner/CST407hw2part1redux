2aea4ca4a26d1e6a0d8788f40ba154a69ebee23b
russellmzauner <russellmzauner@gmail.com>
2014-07-22 21:46:27 -0700

    using /res
    Orientation changes now handled using automatic resource selection in
    Android.

---
+++ gen/edu/oit/cst407/mypreference2/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.oit.cst407.mypreference2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/oit/cst407/mypreference2/OpinionActivity.java
@@ -0,0 +1,21 @@
+package edu.oit.cst407.mypreference2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class OpinionActivity extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+			
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_opinion);
+		
+		
+		
+	}
+
+}
